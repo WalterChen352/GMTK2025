378c240dfa4e35a8bb36d4c7b678062e2b7e7691
theTECmachine <teddy.eddy.c@gmail.com>
2025-08-02 11:22:02 -0700

    sprite billboarding

--- Assets/Scripts/SpriteBillboard.cs
+++ Assets/Scripts/SpriteBillboard.cs
@@ -6,12 +6,14 @@ public class SpriteBillboard : MonoBehaviour
 
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        Vector3 CamForward = cam.forward;
+        CamForward.y = 0;
+        transform.forward = CamForward;
         // cam.y = 0;
         // transform.LookAt(cam);
     }

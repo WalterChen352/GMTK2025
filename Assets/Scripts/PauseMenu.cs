71198830f69da3de25d7662b1b17fcf66405e6a8
RuralBrick <48705917+RuralBrick@users.noreply.github.com>
2025-08-02 12:20:20 -0700

    Music Settings

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,80 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UIElements;
+
+public class PauseMenu : MonoBehaviour
+{
+    static PauseMenu instance;
+
+    Canvas canvas;
+
+    [SerializeField] AudioMixerGroup masterGroup;
+    [SerializeField] AudioMixerGroup musicGroup;
+    [SerializeField] AudioMixerGroup sfxGroup;
+    [SerializeField] AudioMixerGroup uiGroup;
+
+    public static PauseMenu GetInstance()
+    {
+        return instance;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        canvas.enabled = true;
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1f;
+        canvas.enabled = false;
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        masterGroup.audioMixer.SetFloat("MasterVolume", VolumeToDecibel(volume));
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicGroup.audioMixer.SetFloat("MusicVolume", VolumeToDecibel(volume));
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxGroup.audioMixer.SetFloat("SFXVolume", VolumeToDecibel(volume));
+    }
+
+    public void SetUIVolume(float volume)
+    {
+        uiGroup.audioMixer.SetFloat("UIVolume", VolumeToDecibel(volume));
+    }
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+
+    float VolumeToDecibel(float volume)
+    {
+        const float minDb = -80.0f;
+        if (volume <= 0.0001f) // avoid log(0)
+            return minDb;
+
+        float db = 20.0f * (float)Math.Log10(volume);
+        return Math.Max(db, minDb);
+    }
+}

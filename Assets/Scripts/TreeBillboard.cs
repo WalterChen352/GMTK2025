dc9be10eecd39d3425dc041a7dadc651076d859f
WalterChen352 <111105449+WalterChen352@users.noreply.github.com>
2025-08-02 22:52:55 -0700

    Tree stuff

--- Assets/Scripts/TreeBillboard.cs
+++ Assets/Scripts/TreeBillboard.cs
@@ -3,31 +3,24 @@ using UnityEngine;
 public class TreeBillboard : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] Transform cam;
-    private Transform parentTransform;
-
+    Transform cam;
+    public float Offset;
     void Start()
     {
-        parentTransform = transform.parent;
-        if (cam == null) cam = Camera.main.transform;
     }
 
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
     void LateUpdate()
     {
-        if (parentTransform == null || cam == null) return;
-
-        // Get parent's Y rotation only (for 2D-style sprites)
-        float parentYRotation = parentTransform.eulerAngles.y;
-
-        // Face the camera but maintain parent's Y rotation
-        Vector3 lookDirection = cam.position - transform.position;
-        lookDirection.y = 0; // Keep sprite upright
-
-        transform.rotation = Quaternion.LookRotation(lookDirection) *
-                            Quaternion.Euler(0, parentYRotation, 0);
-
-        // Alternative: Full 3D alignment with parent
-        // transform.rotation = parentTransform.rotation * 
-        //                    Quaternion.LookRotation(lookDirection);
+        Vector3 CamForward = cam.forward;
+        //CamForward.y = 0;
+        transform.forward = CamForward;
+        // cam.y = 0;
+        // transform.LookAt(cam);
     }
 }
--- Assets/Scripts/TreeState.cs
+++ Assets/Scripts/TreeState.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TreeState : MonoBehaviour, IInteractable
 {
     public int Hitpoints;
     public WoodCounter woodCounter;
     public GameObject beaver;
+    [SerializeField] List<Sprite> Sprites = new List<Sprite>();
+    public SpriteRenderer Sprite;
     public bool IsInteractable { get; set; }
     public Outline outline;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int Health;
     public Rigidbody TrunkRB;
-    public float Push = 50f;
-    
+    public float Push = 100f;
+
+    [Header("Tree Parts")]
+    public GameObject fullTree; 
+    public GameObject trunk;  
+    public GameObject stump;    
+
     void Start()
     {
         beaver = GameObject.Find("Beaver");
@@ -21,6 +29,23 @@ public class TreeState : MonoBehaviour, IInteractable
         Hitpoints = 4;
         IsInteractable = true;
         outline.enabled = false;
+        //
+        //Sprite = GetComponentInChildren<SpriteRenderer>();
+        if (trunk != null)
+        {
+            TrunkRB = trunk.GetComponent<Rigidbody>();
+            if (TrunkRB != null)
+            {
+                TrunkRB.isKinematic = true;
+                TrunkRB.useGravity = false;
+            }
+        }
+
+        // Ensure full tree is active and parts are disabled initially
+        if (fullTree != null) fullTree.SetActive(true);
+        if (trunk != null) trunk.SetActive(false);
+        if (stump != null) stump.SetActive(false);
+
     }
 
     public void Initialize(int health)
@@ -66,11 +91,40 @@ public class TreeState : MonoBehaviour, IInteractable
     }
     public void TreeFall()
     {
-        TrunkRB.isKinematic = false;
-        TrunkRB.useGravity = true;
-         Vector3 FallDirection = -1* (beaver.transform.position-this.transform.position);
-        TrunkRB.constraints = RigidbodyConstraints.None;
-        TrunkRB.AddForce(FallDirection.normalized * Push, ForceMode.Impulse);
+        if (fullTree != null)
+        {
+            // Get the fulltree sprite position and rotation
+            Vector3 treePosition = fullTree.transform.position;
+            Quaternion treeRotation = fullTree.transform.rotation;
+
+            // Disable the full tree
+            fullTree.SetActive(false);
+
+            if (stump != null)
+            {
+                stump.SetActive(true);
+                // Set stump to the same position/rotation as the full tree
+                stump.transform.position = treePosition;
+                stump.transform.rotation = treeRotation;
+            }
+
+            if (trunk != null)
+            {
+                trunk.SetActive(true);
+                // Set trunk to the same position/rotation as the full tree
+                trunk.transform.position = treePosition;
+                trunk.transform.rotation = treeRotation;
+
+                // Enable physics for the trunk
+                if (TrunkRB != null)
+                {
+                    TrunkRB.isKinematic = false;
+                    TrunkRB.useGravity = true;
+                    // Apply force to make the trunk fall
+                    TrunkRB.AddForce(beaver.transform.forward * Push, ForceMode.Impulse);
+                }
+            }
+        }
 
         IsInteractable = false;
         outline.enabled = false;

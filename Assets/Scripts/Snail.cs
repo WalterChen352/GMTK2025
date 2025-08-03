ad9f761425a849756f3f242f2ab0e1fd2538ee55
RuralBrick <48705917+RuralBrick@users.noreply.github.com>
2025-08-02 22:01:44 -0700

    Snail movement

---
+++ Assets/Scripts/Snail.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+
+public class Snail : MonoBehaviour, IInteractable
+{
+    [SerializeField] float minStartWaitTime;
+    [SerializeField] float maxStartWaitTime;
+    [SerializeField] float minWaitTime;
+    [SerializeField] float maxWaitTime;
+    [SerializeField] float moveTime;
+    [SerializeField] float moveSpeed;
+
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+    Outline outline;
+
+    bool moving = false;
+    float timeTilMove = 0f;
+    float timeTilStop = 0f;
+    Vector3 moveDirection = Vector3.zero;
+
+    public bool IsInteractable { get; set; } = true;
+
+    public void Interact()
+    {
+        if (IsInteractable)
+        {
+            Debug.Log("Snail interacted with");
+        }
+    }
+
+    public void Highlight(bool on)
+    {
+        if (on && IsInteractable)
+        {
+            animator.SetBool("Moving", false);
+            timeTilMove = float.MaxValue;
+            moving = false;
+            outline.enabled = true;
+        }
+        else
+        {
+            if (!moving) {
+                timeTilMove = Random.Range(minWaitTime, maxWaitTime);
+                moveDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
+            }
+            outline.enabled = false;
+        }
+    }
+
+    private void Start()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
+        outline = GetComponentInChildren<Outline>();
+        outline.enabled = false;
+        timeTilMove = Random.Range(minStartWaitTime, maxStartWaitTime);
+    }
+
+    private void Update()
+    {
+        if (moving)
+        {
+            timeTilStop -= Time.deltaTime;
+            if (timeTilStop <= 0f)
+            {
+                animator.SetBool("Moving", false);
+                timeTilMove = Random.Range(minWaitTime, maxWaitTime);
+                moving = false;
+            }
+            else
+            {
+                if (moveDirection.x > 0f)
+                {
+                    spriteRenderer.flipX = true;
+                }
+                else
+                {
+                    spriteRenderer.flipX = false;
+                }
+                transform.position += moveDirection.normalized * Time.deltaTime * moveSpeed;
+            }
+        }
+        else
+        {
+            timeTilMove -= Time.deltaTime;
+            if (timeTilMove <= 0f)
+            {
+                animator.SetBool("Moving", true);
+                timeTilStop = moveTime;
+                moving = true;
+                moveDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f));
+            }
+        }
+    }
+}
